63323a0e8ff613cfc298b2dc0d9a3a95c1a5d08d
Niklas Voss <niklas.voss@gmail.com>
2023-07-29 20:33:33 +0200

    Basic nix environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "go-htmx-alpinejs-tailwind-example";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays = [
+        (final: prev: {
+          # currently none
+        })
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        devShell = pkgs.mkShell rec {
+          name = "go-htmx-alpinejs-tailwind-example";
+
+          buildInputs = with pkgs; [
+            go
+            golangci-lint
+            delve
+            gnumake
+          ];
+        };
+      }
+    );
+}
